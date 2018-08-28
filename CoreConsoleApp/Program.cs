41cc480e1f3ecb8beb673239a21b7e9883d9f209
JT <vs.jeet@gmail.com>
2018-08-28 15:22:36 +0530

    Initial Commit

---
+++ CoreConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the file Name", ConsoleColor.Red);
+            var input = Console.ReadLine();
+            Console.WriteLine($"File Name " + input, ConsoleColor.Green);
+        }
+    }
+}
